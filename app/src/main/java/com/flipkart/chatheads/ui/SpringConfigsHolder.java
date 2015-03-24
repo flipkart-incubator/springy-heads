44f7f9b66370621d1aeab5af8fb68a9acea54892
Kiran Kumar <kiran.kumar@flipkart.com>
2015-03-24 23:56:31 +0530

    chat head speed adjustments

--- app/src/main/java/com/flipkart/chatheads/ui/SpringConfigsHolder.java
+++ app/src/main/java/com/flipkart/chatheads/ui/SpringConfigsHolder.java
@@ -6,6 +6,6 @@ import com.facebook.rebound.SpringConfig;
  * Created by kirankumar on 13/02/15.
  */
 public class SpringConfigsHolder {
-    public static SpringConfig CONVERGING = SpringConfig.fromOrigamiTensionAndFriction(100, 8);
+    public static SpringConfig CONVERGING = SpringConfig.fromOrigamiTensionAndFriction(30, 8);
     public static SpringConfig COASTING = SpringConfig.fromOrigamiTensionAndFriction(0, 2);
 }
